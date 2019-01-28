d277e5c55765019ab4b6fdd1a79ff5290276e98d
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-28 20:07:02 +0100

    Initial commit

---
+++ src/main/java/com/bjornspetprojects/microservices/unitconversionnetflixeurekanamingservice/UnitConversionNetflixEurekaNamingServiceApplication.java
@@ -0,0 +1,14 @@
+package com.bjornspetprojects.microservices.unitconversionnetflixeurekanamingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitConversionNetflixEurekaNamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitConversionNetflixEurekaNamingServiceApplication.class, args);
+	}
+
+}
+
