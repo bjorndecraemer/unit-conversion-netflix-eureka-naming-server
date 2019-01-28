01a352550163aed953bb54fd0664ed48bb9637b8
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-28 20:29:11 +0100

    Finalize Eureka naming server

--- src/main/java/com/bjornspetprojects/microservices/unitconversionnetflixeurekanamingservice/UnitConversionNetflixEurekaNamingServiceApplication.java
+++ src/main/java/com/bjornspetprojects/microservices/unitconversionnetflixeurekanamingservice/UnitConversionNetflixEurekaNamingServiceApplication.java
@@ -2,8 +2,10 @@ package com.bjornspetprojects.microservices.unitconversionnetflixeurekanamingser
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class UnitConversionNetflixEurekaNamingServiceApplication {
 
 	public static void main(String[] args) {
